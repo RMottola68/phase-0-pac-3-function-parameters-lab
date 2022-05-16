4dd20334402ece90f119f54a5467ab327adac39b
Ryan Mottola <rmottola68@gmail.com>
2022-05-16 19:52:17 -0400

    commit tree 37116a0e0663705a6f11b53076f52ceecca9922a

---
+++ index.js
@@ -0,0 +1,11 @@
+let introduction = (name) => {
+    return `Hi, my name is ${name}.`;
+}
+
+let introductionWithLanguage = (name, language) => {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
+
+let introductionWithLanguageOptional = (name, language = 'JavaScript') => {
+    return `Hi, my name is ${name} and I am learning to program in ${language}.`
+}
